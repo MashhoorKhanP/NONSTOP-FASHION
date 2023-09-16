f70ce073ec443182d521cf07d388cce14a38fd24
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2023-09-16 16:18:38 +0530

    banner problem solved

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -1,8 +1,7 @@
 const Banner = require('../models/bannerModel');
-const bannerCompare = require('../models/bannerModel')
 const Admin = require('../models/adminModel');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs')
 const { nextTick } = require('process');
 /** Get Banner Start */
 const getBanners = async (req, res, next) => {
@@ -18,36 +17,41 @@ const getBanners = async (req, res, next) => {
 /** Post Banner Start */
 const postUpdateBanner = async (req, res, next) => {
     try {
-        const { name, url } = req.body;
-        //console.log(name, url);
-        let image = false;
+        const { name, url } = req.body
+
+        let image = false
         if (req.file) {
-            image = req.file.filename;
+            image = req.file.filename
         }
-        //console.log(name, url, image);
-        const bannerExist = await bannerCompare.findOne({ name })
+
+        const bannerExist = await Banner.findOne({ name })
         if (bannerExist && image != false && url != null) {
-            await fs.unlink(path.join(__dirname, '../public/assets/banner-images') + name, (error) => {
-                if (error) {
-                    next(error);
+            await fs.unlink(path.join(__dirname, '../public/assets/banner-images/') + bannerExist.image, (err) => {
+                if (err) {
+                    next(err)
                 }
             });
-            await Banner.updateOne({ name }, { $set: { image, url } });
-        } else if (bannerExist && image != false) {
-            await fs.unlink(path.join(__dirname, '../public/assets/banner-images') + name, (error) => {
-                if (error) {
-                    next(error);
+            await Banner.updateOne({ name }, { $set: { image, url } })
+        }
+        else if (bannerExist && image != false) {
+            await fs.unlink(path.join(__dirname, '../public/assets/banner-images/') + bannerExist.image, (err) => {
+                if (err) {
+                    next(err)
                 }
             });
             await Banner.updateOne({ name }, { $set: { image } })
-        } else if (bannerExist && url != null) {
-            await Banner.updateOne({ name }, { $set: { url } })
-        } else {
-            await new Banner({ name, image, url }).save();
         }
-        return res.redirect('/admin/banners');
+        else if (bannerExist && url != null) {
+            await Banner.updateOne({ name }, { $set: { url } })
+        }
+        else {
+            await new Banner({ name, image, url }).save()
+            return res.redirect('/admin/banners');
+        }
+        return res.redirect('/admin/banners')
+
     } catch (error) {
-        console.log(error);
+        next(error)
     }
 }
 /** Post Banner End */
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.COLLECTION);
+mongoose.connect(process.env.MONGO_CONNECT);
 const path = require('path');
 const morgan = require('morgan');
 
